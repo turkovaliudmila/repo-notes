0765295ad60e45d8e99b2cb770fbaa20d0f8b74b
turkovaliudmila <ljudmila327@rambler.ru>
2021-03-20 20:34:44 +0300

    Создан активити для отображения только полей заметки и макет этой активити.

---
+++ app/src/main/java/ru/geekbrains/appnotes/DetailsActivity.java
@@ -0,0 +1,38 @@
+package ru.geekbrains.appnotes;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.text.TextUtils;
+
+import static android.text.TextUtils.replace;
+
+
+public class DetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details);
+
+        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            finish();
+            return;
+        }
+
+        if (savedInstanceState == null) {
+            DetailsNoteFragment detailsFragment = new DetailsNoteFragment();
+            detailsFragment.setArguments(getIntent().getExtras());
+
+            FragmentTransaction fragmentTransaction = getSupportFragmentManager()
+                    .beginTransaction();
+
+            // добавляем фрагмент
+            DetailsNoteFragment fragment= new DetailsNoteFragment();
+            fragmentTransaction.add(R.id.fragment_container, detailsFragment);
+            fragmentTransaction.commit();
+        }
+    }
+}
